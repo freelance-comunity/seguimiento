082c93717893941ec0e25c967fc4053f53638511
jcmontejo <jncrlsmontejo@gmail.com>
2018-01-03 08:52:02 -0600

    Se añadio AdminLTE a Laravel 5.5.*

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // LaravelAdminLte
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
 
     ],
 
